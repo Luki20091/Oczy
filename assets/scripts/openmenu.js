490caa2c3fd6f5fe398d17217d7cc2a1c9424e2e
Luki20091 <76406872+Luki20091@users.noreply.github.com>
2022-05-03 18:03:58 +0200

    Add files via upload

---
+++ assets/scripts/openmenu.js
@@ -0,0 +1,26 @@
+function openMenu() {
+    document.getElementById("menu-items").classList.toggle("menu-items-visible");
+}
+
+function closeMenu() {
+    document.getElementById("menu-items").classList.remove("menu-items-visible");
+    document.getElementById("toggle").checked = false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -1,30 +1,40 @@
 $("a[id='budowa']").click(function () {
     $("html, body, content").animate({ scrollTop: $("#to-budowa").offset().top }, "slow");
     document.title = ('Oczy | Budowa');
+    document.getElementById("menu-items").classList.remove("menu-items-visible");
+    document.getElementById("toggle").checked = false;
     return false;
 });
 
 $("a[id='wady']").click(function () {
     $("html, body, content").animate({ scrollTop: $("#to-wady").offset().top }, "slow");
     document.title = ('Oczy | Wady');
+    document.getElementById("menu-items").classList.remove("menu-items-visible");
+    document.getElementById("toggle").checked = false;
     return false;
 });
 
 $("a[id='ochrona']").click(function () {
     $("html, body, content").animate({ scrollTop: $("#to-ochrona").offset().top }, "slow");
     document.title = ('Oczy | Ochrona');
+    document.getElementById("menu-items").classList.remove("menu-items-visible");
+    document.getElementById("toggle").checked = false;
     return false;
 });
 
 $("a[id='fakty']").click(function () {
     $("html, body, content").animate({ scrollTop: $("#to-fakty").offset().top }, "slow");
     document.title = ('Oczy | Fakty');
+    document.getElementById("menu-items").classList.remove("menu-items-visible");
+    document.getElementById("toggle").checked = false;
     return false;
 });
 
 $("a[id='ciekawostki']").click(function () {
     $("html, body, content").animate({ scrollTop: $("#to-ciekawostki").offset().top }, "slow");
     document.title = ('Oczy | Ciekawostki');
+    document.getElementById("menu-items").classList.remove("menu-items-visible");
+    document.getElementById("toggle").checked = false;
     return false;
 });
 
@@ -37,6 +47,8 @@ $("a[id='glowna']").click(function () {
 
 if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
     $("html, body, content").animate({ scrollTop: $("#to-glowna").offset().top });
+    document.getElementById("menu-items").classList.remove("menu-items-visible");
+    document.getElementById("toggle").checked = false;
 };
 
 
