dd8a69d0687be3be5e3060fdfc21c5c3cb27bbfe
Luki20091 <76406872+Luki20091@users.noreply.github.com>
2022-05-04 17:28:39 +0200

    Add files via upload

--- assets/scripts/openmenu.js
+++ assets/scripts/openmenu.js
@@ -1,11 +1,13 @@
 function openMenu() {
     document.getElementById("menu-items").classList.toggle("menu-items-visible");
-}
+    return false;
+};
 
 function closeMenu() {
     document.getElementById("menu-items").classList.remove("menu-items-visible");
     document.getElementById("toggle").checked = false;
-}
+    return false;
+};
 
 
 
